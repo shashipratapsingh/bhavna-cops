043e7b346e8000b76ad644539ed610b6dcc60aee
itpro8577@gmail.com <itpro8577@gmail.com>
2025-01-04 22:50:19 +0530

    added some field in model

--- src/main/java/consumer/service/entity/Product.java
+++ src/main/java/consumer/service/entity/Product.java
@@ -1,7 +1,10 @@
 package consumer.service.entity;
 
+import jakarta.persistence.*;
 import jakarta.validation.constraints.NotEmpty;
 import lombok.*;
+
+import java.util.Date;
 
 @Data
 @AllArgsConstructor
@@ -16,5 +19,25 @@ public class Product {
     private String name;
 
     private double price;
+    private int quantity;
+    private String description;
+
+    @Column(nullable = false, updatable = false)
+    @Temporal(TemporalType.TIMESTAMP)
+    private Date createdBy;
+
+    @Temporal(TemporalType.TIMESTAMP)
+    private Date updatedBy;
+
+    @PrePersist
+    protected void onCreate() {
+        this.createdBy = new Date();
+        this.updatedBy = new Date();
+    }
+
+    @PreUpdate
+    protected void onUpdate() {
+        this.updatedBy = new Date();
+    }
 
 }

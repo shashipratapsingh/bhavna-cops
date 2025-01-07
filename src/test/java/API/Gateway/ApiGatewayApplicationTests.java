f5dab8e7a400ffbbc6a735532e1c8a6705379717
itpro8577@gmail.com <itpro8577@gmail.com>
2025-01-07 10:28:12 +0530

    api getway first commit

---
+++ src/test/java/API/Gateway/ApiGatewayApplicationTests.java
@@ -0,0 +1,15 @@
+/*
+package API.Gateway;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiGatewayApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
+*/

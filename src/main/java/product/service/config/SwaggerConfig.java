0c173ff0504a25df6fd681edb9629b9a9d185694
itpro8577@gmail.com <itpro8577@gmail.com>
2025-01-10 17:03:28 +0530

    swagger

--- src/main/java/product/service/config/SwaggerConfig.java
+++ src/main/java/product/service/config/SwaggerConfig.java
@@ -1,11 +1,23 @@
 package product.service.config;
 
 
+import io.swagger.v3.oas.annotations.enums.SecuritySchemeIn;
+import io.swagger.v3.oas.annotations.enums.SecuritySchemeType;
+import io.swagger.v3.oas.annotations.security.SecurityScheme;
 import io.swagger.v3.oas.models.OpenAPI;
 import io.swagger.v3.oas.models.info.Info;
 import io.swagger.v3.oas.models.info.License;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+
+@SecurityScheme(
+        name = "bearerAuth",
+        description = "JWT auth description",
+        scheme = "bearer",
+        type = SecuritySchemeType.HTTP,
+        bearerFormat = "JWT",
+        in= SecuritySchemeIn.HEADER
+)
 
 @Configuration
 public class SwaggerConfig {
--- src/main/java/product/service/entity/Product.java
+++ src/main/java/product/service/entity/Product.java
@@ -16,9 +16,13 @@ public class Product {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
+    @Column(nullable = false)
     private String name;
+    @Column(nullable = false)
     private double price;
+    @Column(nullable = false)
     private int quantity;
+    @Column(nullable = false)
     private String description;
 
     @Column(nullable = false, updatable = false)
